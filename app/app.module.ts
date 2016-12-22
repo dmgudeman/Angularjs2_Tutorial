03a9e12f84045b42a48647ce89227e2cfc888db8
davidmgudeman <davidmgudeman@gmail.com>
2016-12-22 09:36:27 -0800

    Started input exercises

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ClickMeComponent } from './click-me.component';
+import {KeyUpComponent_v1} from './keyup.components';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+                  ClickMeComponent,
+                  KeyUpComponent_v1],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
